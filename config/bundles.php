a031b5a9e4eb07b7aee441a9a11dc957b3d2f3f6
Antoine DEGUIN <antoine.deguin@supinternet.fr>
2018-12-10 11:49:22 +0100

    fix error

--- config/bundles.php
+++ config/bundles.php
@@ -10,6 +10,6 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true],
     Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
 ];
